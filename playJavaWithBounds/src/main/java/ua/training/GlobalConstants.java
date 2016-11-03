2320db7083792fc7740979cc7f9cb48917e98a1a
taraslazoryk <taraslazoryk@gmail.com>
2016-11-04 00:26:48 +0200

    add global constants

---
+++ playJavaWithBounds/src/main/java/ua/training/GlobalConstants.java
@@ -0,0 +1,25 @@
+package ua.training;
+/**
+ * GlobalConstants is an Interface which contains all Global constants.
+ * 
+ * @version 1.0 30 Oct 2016
+ * @author taras
+ *
+ */
+public interface GlobalConstants {
+	String INPUT_INT_DATA_BETWEEN = "Write INT value between ";
+	String INPUT_INT_DATA = "Write INT value";
+	String WRONG_INPUT_INT_DATA_BETWEEN = "Wrong input data. Write Int value between ";
+	String WRONG_INPUT_INT_DATA = "Wrong input data. Write Int value ";
+	String WRONG_BOUNDARIES_INT_DATA = "Data out of boundaries. Write Int value between ";
+	String EXIT_CONDITION_MESSAGE = "Write \"exit\" to exit the game";
+	String CORRECT_VALUE = "Congratulations you guessed!!";
+	String INCORECT_BOUNDARIES = "Lower boundary must have smaller value the bigger";
+	String TYPE_LOWER_BOUNDARY = "Type lower boundary please :";
+	String TYPE_UPPER_BOUNDARY = "Type upper boundary please :";	
+	String TRIES_AMOUNT = "Amount of tries = ";
+	String EXIT_VALUE = "exit";
+	String AND = " and ";
+	String EQUALS = " = ";
+	String TRY_WAS = " try was ";
+}
