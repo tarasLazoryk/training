2b145514a809a9155cb2f73faf977e19b774cf3b
taraslazoryk <taraslazoryk@gmail.com>
2016-11-04 00:27:22 +0200

    edited comments and package name

--- playJavaWithBounds/src/main/java/ua/training/Controller.java
+++ playJavaWithBounds/src/main/java/ua/training/Controller.java
@@ -1,82 +1,141 @@
-package ua.playjava;
+package ua.training;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Controller class. It expresses for controller in MVC pattern.
+ * Controller class. It expresses controller in MVC pattern.
  * 
  * @version 1.0 30 Oct 2016
  * @author Taras Lazoryk
  *
  */
-public class Controller {	
-	private Model model = new Model();
-	private View view = new View();
-	private ArrayList<Integer> tryList = new ArrayList<Integer>();
-
-	//constructor
+public class Controller {
+	Model model = new Model();
+	View view = new View();
+	// constructor
 	public Controller(Model model, View view) {
 		this.model = model;
 		this.view = view;
 
 	}
 
-	/** 
-	 * Check if value from scanner is integer, or not
-	 * If it is not, ask to input new value 
-	 */
-	public int inputIntValue(Scanner sc) {
-		while (!sc.hasNextInt()) {
-			view.printWithBounds(View.WRONG_INPUT_INT_DATA, 
-								model.getMinValue(), model.getMaxValue());
-			sc.next();
-		}
-		return sc.nextInt();
-	}
-	
-	/** 
-	 * Check if value from scanner is in bounds, or not
-	 * If it is not, ask to input new value 
-	 */
-	public int isValueInBounds(Scanner sc) {
-		int userValue = inputIntValue(sc);
-		
-		while (!model.isInBounds(userValue)) {
-			view.printWithBounds(View.WRONG_INPUT_INT_DATA, model.getMinValue(),
-								model.getMaxValue());
-			userValue = inputIntValue(sc);
-		}
-		
-		tryList.add(userValue);
-		return userValue;
-	}
-	
-	/** 
-	 * Ask user to input integer number which lies in bounds,
-	 * check with isValueInBounds() and inputIntValue() methods
-	 * When user guess, write the result
+	/**
+	 * The main method, which asks user to type value in the bounds, until user
+	 * doesn't guess the number, or until he typed word "exit" - which will stop
+	 * the program.
 	 */
 	public void playGame() {
 		Scanner sc = new Scanner(System.in);
-		
-		view.printWithBounds(View.INPUT_INT_DATA, model.getMinValue(), 
-							model.getMaxValue());
-		model.setUserValue(isValueInBounds(sc));
+		setBoundaries(sc);
+		//model.setBoundaries(GlobalConstants.MIN_VALUE, GlobalConstants.MAX_VALUE);
 
-		while (!model.isEqualToProgramValue(model.getUserValue())) {
-			if (model.isBiggerThenProgramValue(model.getUserValue())) {
-				view.printWithBounds(View.BIGGER_VALUE, model.getMinValue(),
-									model.getMaxValue());
-				model.setUserValue(isValueInBounds(sc));
-			} else {
-				view.printWithBounds(View.SMALLER_VALUE, model.getMinValue(), 
-									model.getMaxValue());
-				model.setUserValue(isValueInBounds(sc));
-			}
+		model.setSecretValue(model.getLowerBoundary(), model.getUpperBoundary());
+		view.print(GlobalConstants.EXIT_CONDITION_MESSAGE);
+		while (!model.checkValue(checkIsInputValueInBounds(sc))) {
 		}
-		
-		view.print(View.CORRECT_VALUE);
-		view.printStatistic(tryList);
+
+		view.print(GlobalConstants.CORRECT_VALUE);
+		view.printStatistic(model.getTryList());
 	}
+
+	/**
+	 * Check is the value typed by user via scanner is a number or not It will
+	 * ask user to write a number until he does it, or until he would write
+	 * "exit" - in this case the program will be stopped.
+	 * 
+	 * @param sc - the scanner from which value is gotten
+	 * @return integer value.
+	 */
+	public int inputIntValueWithScanner(Scanner sc) {
+		String scannerValue = "";
+		if(model.getLowerBoundary() == model.getUpperBoundary()){
+			view.print(GlobalConstants.INPUT_INT_DATA);
+		} else {
+			view.print(GlobalConstants.INPUT_INT_DATA_BETWEEN 
+					+ model.getLowerBoundary() + GlobalConstants.AND 
+					+ model.getUpperBoundary() + GlobalConstants.EQUALS);			
+		}	
+		scannerValue = sc.next();
+		while (true) {
+			while (!checkStringForInt(scannerValue)) {
+				if (scannerValue.equals(GlobalConstants.EXIT_VALUE)) {
+					System.exit(0);
+				} else {
+					if(model.getLowerBoundary() == model.getUpperBoundary()){
+						view.print(GlobalConstants.WRONG_INPUT_INT_DATA);
+					} else {
+						view.print(GlobalConstants.WRONG_INPUT_INT_DATA_BETWEEN 
+								+ model.getLowerBoundary() + GlobalConstants.AND
+								+ model.getUpperBoundary() + GlobalConstants.EQUALS);			
+					}	
+					scannerValue = sc.next();
+				}
+
+			}
+			return Integer.parseInt(scannerValue);
+		}
+	}
+
+	/**
+	 * Asks to write the value which lies in the bounds. Takes values from the
+	 * scanner, and will ask to write a value in the bounds until user does it.	 * 
+	 * @param sc - the scanner from which value is gotten.
+	 * @return return value which lies in the bounds.
+	 */
+	public int checkIsInputValueInBounds(Scanner sc) {
+		int value = inputIntValueWithScanner(sc);
+		while (value <= model.getLowerBoundary() || value >= model.getUpperBoundary()) {
+
+			view.print(GlobalConstants.WRONG_BOUNDARIES_INT_DATA 
+					+ model.getLowerBoundary() + GlobalConstants.AND 
+					+ model.getUpperBoundary() + GlobalConstants.EQUALS);
+			value = inputIntValueWithScanner(sc);
+		}
+		return value;
+	}
+	
+	/**
+	 * Asks user to write lower and upper boundaries of the interval
+	 * to the scanner. It will ask to write boundaries until used wouldn't
+	 * do it correctly. Upper boundary must be bigger then lower.
+	 *  
+	 * @param sc - scanner from which data is gotten.
+	 * @return returns true if lower boundary is smaller then upper.
+	 */
+	public boolean setBoundaries(Scanner sc){
+		int lowerBoundary = 0;
+		int upperBoundary = 0;
+		view.print(GlobalConstants.TYPE_LOWER_BOUNDARY);
+		lowerBoundary = inputIntValueWithScanner(sc);
+		view.print(GlobalConstants.TYPE_UPPER_BOUNDARY);
+		upperBoundary = inputIntValueWithScanner(sc);
+		while(lowerBoundary >= upperBoundary){
+			view.print(GlobalConstants.INCORECT_BOUNDARIES);
+			view.print(GlobalConstants.TYPE_LOWER_BOUNDARY);
+			lowerBoundary = inputIntValueWithScanner(sc);
+			view.print(GlobalConstants.TYPE_UPPER_BOUNDARY);
+			upperBoundary = inputIntValueWithScanner(sc);
+			
+		}
+		model.setBoundaries(lowerBoundary, upperBoundary);
+		return true;	
+		
+	}
+
+	/**
+	 * Checks is the string can be converted into the integer. 
+	 * 
+	 * @param string - string to be checked.
+	 * @return return true if string can be converted and false if it is not.
+	 */
+	public boolean checkStringForInt(String string) {
+		try {
+			Integer.parseInt(string);
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
 }
--- playJavaWithBounds/src/main/java/ua/training/View.java
+++ playJavaWithBounds/src/main/java/ua/training/View.java
@@ -1,38 +1,34 @@
-package ua.playjava;
+package ua.training;
 
 import java.util.ArrayList;
 
 /**
- * View class. It expresses for view in MVC pattern.
+ * View class. It expresses view in MVC pattern.
  * 
  * @version 1.0 30 Oct 2016
  * @author Taras Lazoryk
  *
  */
 public class View {
-	// text constants
-	public static final String INPUT_INT_DATA = "Write INT value between ";
-	public static final String WRONG_INPUT_INT_DATA = "Wrong input data. Write INt value between ";
-	public static final String BIGGER_VALUE = "Yours value is bigger then mine. Write INT value between ";
-	public static final String SMALLER_VALUE = "Yours value is smaller then mine. Write INT value between ";
-	public static final String CORRECT_VALUE = "Congratulations you guessed!!";
-	public static final String TRIES_AMOUNT = "Amount of tries = ";
-	public static final String AND = " and ";
-	public static final String EQUALS = " = ";
-	public static final String TRY_WAS = " try was ";
-	
+	/**
+	 * This method prints a string.  
+	 * 
+	 * @param message - The String to be printed.
+	 */
 	public void print(String message) {
 		System.out.println(message);
-	}
-
-	public void printWithBounds(String message, int minValue, int maxValue) {
-		System.out.println(message + minValue + AND + maxValue + EQUALS);
-	}
-
+	}	
+	/**
+	 * This method prints every element of ArrayList<Integer> in new line. 
+	 * It prints it in form: <Number of element> "try was" <Value of element>
+	 * and then terminate the line. 
+	 * 
+	 * @param tryList - The ArrayList<Integer> to be printed.
+	 */
 	public void printStatistic(ArrayList<Integer> tryList) {
-		System.out.println(TRIES_AMOUNT + tryList.size());
+		System.out.println(GlobalConstants.TRIES_AMOUNT + tryList.size());
 		for (int i = 0; i < tryList.size(); i++) {
-			System.out.println(i + 1 + TRY_WAS + tryList.get(i));
+			System.out.println(i + 1 + GlobalConstants.TRY_WAS + tryList.get(i));
 		}
 
 	}
