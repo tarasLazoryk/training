14c8c847afadde0e184844f6854ca759b84897a8
taraslazoryk <taraslazoryk@gmail.com>
2016-11-03 23:55:55 +0200

    dynamic boundaries (reverted from commit b7edcfab706c99303fca957e52461da0e000e1b4)

--- playJava/src/main/java/ua/training/Controller.java
+++ playJava/src/main/java/ua/training/Controller.java
@@ -19,16 +19,16 @@ public class Controller {
 		this.view = view;
 
 	}
-
+	
 	/**
-	 * The main method, which asks user to type value in the bounds, until user
-	 * doesn't guess the number, or until he typed word "exit" - which will stop
-	 * the program.
+	 * The main method, which asks user to type value in the bounds,
+	 * until user doesn't guess the number,
+	 * or until he typed word "exit" - which will stop the program.
 	 */
 	public void playGame() {
 		Scanner sc = new Scanner(System.in);
-		setBoundaries(sc);
-		//model.setBoundaries(GlobalConstants.MIN_VALUE, GlobalConstants.MAX_VALUE);
+
+		model.setBoundaries(GlobalConstants.MIN_VALUE, GlobalConstants.MAX_VALUE);
 
 		model.setSecretValue(model.getLowerBoundary(), model.getUpperBoundary());
 		view.print(GlobalConstants.EXIT_CONDITION_MESSAGE);
@@ -38,36 +38,29 @@ public class Controller {
 		view.print(GlobalConstants.CORRECT_VALUE);
 		view.printStatistic(model.getTryList());
 	}
-
+	
 	/**
-	 * Check is the value typed by user via scanner is a number or not It will
-	 * ask user to write a number until he does it, or until he would write
-	 * "exit" - in this case the program will be stopped.
-	 * @param sc - the scanner from which value is gotten
+	 * Check is the value typed by user via scanner is a number or not 
+	 * It will ask user to write a number until he does it,
+	 * or until he would write "exit" - in this case the program 
+	 * will be stopped. 
+	 * @param sc -  the scanner from which value is gotten
 	 * @return integer value.
 	 */
 	public int inputIntValueWithScanner(Scanner sc) {
 		String scannerValue = "";
-		if(model.getLowerBoundary() == model.getUpperBoundary()){
-			view.print(GlobalConstants.INPUT_INT_DATA);
-		} else {
-			view.print(GlobalConstants.INPUT_INT_DATA_BETWEEN + 
-						model.getLowerBoundary() + GlobalConstants.AND +
-						model.getUpperBoundary() + GlobalConstants.EQUALS);			
-		}	
+		view.print(GlobalConstants.INPUT_INT_DATA + model.getLowerBoundary() + 
+					GlobalConstants.AND +  model.getUpperBoundary() + 
+					GlobalConstants.EQUALS);
 		scannerValue = sc.next();
 		while (true) {
 			while (!checkStringForInt(scannerValue)) {
 				if (scannerValue.equals(GlobalConstants.EXIT_VALUE)) {
 					System.exit(0);
 				} else {
-					if(model.getLowerBoundary() == model.getUpperBoundary()){
-						view.print(GlobalConstants.WRONG_INPUT_INT_DATA);
-					} else {
-						view.print(GlobalConstants.WRONG_INPUT_INT_DATA_BETWEEN 
-								+ model.getLowerBoundary() + GlobalConstants.AND
-								+ model.getUpperBoundary() + GlobalConstants.EQUALS);			
-					}	
+					view.print(GlobalConstants.WRONG_INPUT_INT_DATA + 
+								model.getLowerBoundary() + GlobalConstants.AND +
+								model.getUpperBoundary() + GlobalConstants.EQUALS);
 					scannerValue = sc.next();
 				}
 
@@ -75,10 +68,11 @@ public class Controller {
 			return Integer.parseInt(scannerValue);
 		}
 	}
-
+	
 	/**
-	 * Asks to write the value which lies in the bounds. Takes values from the
-	 * scanner, and will ask to write a value in the bounds until user does it.	 * 
+	 * Asks to write the value which lies in the bounds.
+	 * Takes values from the scanner, and will ask to write
+	 * a value in the bounds until user does it.
 	 * @param sc - the scanner from which value is gotten.
 	 * @return return value which lies in the bounds.
 	 */
@@ -95,34 +89,7 @@ public class Controller {
 	}
 	
 	/**
-	 * Asks user to write lower and upper boundaries of the interval
-	 * to the scanner. It will ask to write boundaries until used wouldn't
-	 * do it correctly. Upper boundary must be bigger then lower. 
-	 * @param sc - scanner from which data is gotten.
-	 * @return returns true if lower boundary is smaller then upper.
-	 */
-	public boolean setBoundaries(Scanner sc){
-		int lowerBoundary = 0;
-		int upperBoundary = 0;
-		view.print(GlobalConstants.TYPE_LOWER_BOUNDARY);
-		lowerBoundary = inputIntValueWithScanner(sc);
-		view.print(GlobalConstants.TYPE_UPPER_BOUNDARY);
-		upperBoundary = inputIntValueWithScanner(sc);
-		while(lowerBoundary >= upperBoundary){
-			view.print(GlobalConstants.INCORECT_BOUNDARIES);
-			view.print(GlobalConstants.TYPE_LOWER_BOUNDARY);
-			lowerBoundary = inputIntValueWithScanner(sc);
-			view.print(GlobalConstants.TYPE_UPPER_BOUNDARY);
-			upperBoundary = inputIntValueWithScanner(sc);
-			
-		}
-		model.setBoundaries(lowerBoundary, upperBoundary);
-		return true;	
-		
-	}
-
-	/**
-	 * Checks is the string can be converted into the integer. 
+	 * Checks is the string can be converted into the integer.
 	 * @param string - string to be checked.
 	 * @return return true if string can be converted and false if it is not.
 	 */
@@ -134,5 +101,4 @@ public class Controller {
 		}
 		return true;
 	}
-
 }
--- playJava/src/main/java/ua/training/GlobalConstants.java
+++ playJava/src/main/java/ua/training/GlobalConstants.java
@@ -6,17 +6,14 @@ package ua.training;
  * @author taras
  *
  */
-public interface GlobalConstants {	
-	String INPUT_INT_DATA_BETWEEN = "Write INT value between ";
-	String INPUT_INT_DATA = "Write INT value";
-	String WRONG_INPUT_INT_DATA_BETWEEN = "Wrong input data. Write Int value between ";
-	String WRONG_INPUT_INT_DATA = "Wrong input data. Write Int value ";
+public interface GlobalConstants {
+	int MAX_VALUE = 100;
+	int MIN_VALUE = 0;
+	String INPUT_INT_DATA = "Write INT value between ";
+	String WRONG_INPUT_INT_DATA = "Wrong input data. Write Int value between ";
 	String WRONG_BOUNDARIES_INT_DATA = "Data out of boundaries. Write Int value between ";
 	String EXIT_CONDITION_MESSAGE = "Write \"exit\" to exit the game";
 	String CORRECT_VALUE = "Congratulations you guessed!!";
-	String INCORECT_BOUNDARIES = "Lower boundary must have smaller value the bigger";
-	String TYPE_LOWER_BOUNDARY = "Type lower boundary please :";
-	String TYPE_UPPER_BOUNDARY = "Type upper boundary please :";	
 	String TRIES_AMOUNT = "Amount of tries = ";
 	String EXIT_VALUE = "exit";
 	String AND = " and ";
